67afbd9c0cade703fa93938cd88f57f4d86d93bd
Your Name <you@example.com>
2023-09-07 19:27:48 +0300

    aloitettiin korttin teko

---
+++ 2023SyksyTo1830_Card_Game/src/App.jsx
@@ -0,0 +1,11 @@
+import Card from './components/Card'
+
+export default function App(){
+  return(
+    <>
+      <h1>Hello world!</h1>
+      <Card/>
+  
+    </>
+  );
+}
---
+++ 2023SyksyTo1830_Card_Game/src/components/Card.jsx
@@ -0,0 +1,10 @@
+
+export default function Card(){
+    return(
+        <div>
+            <img src="http://placekitten.com/120/100"/>
+            <span>Cuteness</span>
+            <span>9</span>
+        </div>
+    );
+}
