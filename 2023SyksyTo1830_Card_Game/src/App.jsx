1d404f897cf8d9dcfff66a1002504a832a06fd0b
Your Name <you@example.com>
2023-09-28 19:26:43 +0300

    createCard ja usestate

--- 2023SyksyTo1830_Card_Game/src/App.jsx
+++ 2023SyksyTo1830_Card_Game/src/App.jsx
@@ -1,9 +1,13 @@
 import Card from './components/Card'
 import './App.css'
+import { useState } from 'react'
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min); 
+
 const playerCard = {
   image: "http://placekitten.com/120/100",
-  stats: [{name:'Cuteness', value: 9},
-          {name:'Speed', value: 50}]
+  stats: [{name:'Cuteness', value: getRandomInt(1,999)},
+          {name:'Speed', value: getRandomInt(1,999)}]
 }
 
 const opponentCard = {
@@ -12,31 +16,46 @@ const opponentCard = {
           {name:'Speed', value: 5}]
 }
 
+const createCard = index =>({
+  image: "http://placekitten.com/120/100?=" + index,
+  stats: [{name:'Cuteness', value: getRandomInt(1,999)},
+          {name:'Speed', value: getRandomInt(1,999)},
+          {name:'Weight', value: getRandomInt(1,999)}
+        ],
+  id: crypto.randomUUID()
+})
+
 export default function App(){
   
+  const [result, setResult] = useState('');
+
   function compareCards(){
     const playerStats = playerCard.stats[0];
     const opponentStats = opponentCard.stats[0];
-    let result = '';
 
     if(playerStats.value === opponentStats.value){
-      result = "draw";
+      setResult('Draw');
     }
     else if(playerStats.value > opponentStats.value){
-      result = "win";
+      setResult('Win');
     }
     else{
-      result = "loss";
+      setResult('Loss');
     }
-    console.log(result);
+
   }
 
   return(
     <>
       <h1>Hello world!</h1>
-      <Card card={playerCard}/>
-      <button type='button' onClick={compareCards} >Play</button>
-      <Card card={opponentCard}/>
+      <div className='game'>
+        <Card card={playerCard}/>
+        <div className='center-area'>
+          <p>{result || 'Press the button'}</p>
+          <button type='button' onClick={compareCards} >Play</button>
+        </div>
+        <Card card={opponentCard}/>
+      </div>
     </>
   );
 }
